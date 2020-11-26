f568b2bb8575c125316ee3307d8681fba086d937
Arav <71561508+zCranking@users.noreply.github.com>
2020-11-26 08:06:43 -0800

    Adding the RGB webpage

---
+++ main.js
@@ -0,0 +1,9 @@
+function red() {
+    document.getElementById("Screen").style.backgroundColor = "Red";
+}
+function blue() {
+    document.getElementById("Screen").style.backgroundColor = "Blue";
+}
+function green() {
+    document.getElementById("Screen").style.backgroundColor = "Green";
+}
